ecb68d50b00b6a7cc6f8a42ae3c4263aefd30e80
Wu Huijie <hjwu85@gmail.com>
2018-10-06 09:50:39 +0800

    more tests with Pointers

--- 20180929-Pointers/main.go
+++ 20180929-Pointers/main.go
@@ -8,6 +8,14 @@ type person struct {
 	gender string
 }
 
+func (p person) tryChangeName(name string) {
+	p.name = name
+}
+
+func (p *person) reallyChangeName(name string) {
+	p.name = name
+}
+
 func main() {
 	joe := person{"Joe", 25, "male"}
 	fmt.Printf("%T, %v\n", joe, joe)
@@ -25,4 +33,16 @@ func main() {
 	fmt.Printf("%T, %v\n", pointer, pointer)
 	(*pointer).name = "JenJen"
 	fmt.Printf("%T, %v\n", pointer, pointer)
+
+	mark := &person{"Mark", 30, "male"}
+	mark.tryChangeName("Marky")
+	fmt.Printf("Mark try to change name to: %s, got: %s\n", "Marky", mark.name)
+	mark.reallyChangeName("Marko")
+	fmt.Printf("Mark try to change name to: %s, got: %s\n", "Marko", mark.name)
+
+	sean := person{"Sean", 30, "male"}
+	sean.tryChangeName("Seano")
+	fmt.Printf("Sean try to change name to: %s, got: %s\n", "Seano", sean.name)
+	sean.reallyChangeName("Seaaan")
+	fmt.Printf("Sean try to change name to: %s, got: %s\n", "Seaaan", sean.name)
 }
